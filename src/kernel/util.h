e4f23e8f4d70408d13ecd779cfea0c22eae81df9
Sam Tebbs <samuel.tebbs@gmail.com>
2018-04-13 11:34:36 +0100

    Finished off PANIC macro

--- src/kernel/util.h
+++ src/kernel/util.h
@@ -11,4 +11,9 @@ void outb(uint16_t port, uint8_t val);
 uint8_t inb(uint16_t port);
 void memset(void* buff, size_t len, char value);
 
+extern void print(char*);
+extern void print_u32(uint32_t);
+
+#define PANIC(str) { print("PANIC: " str " @ " __FILE__ ":"); print_u32(__LINE__); while(1); }
+
 #endif //JAQ_UTIL_H
