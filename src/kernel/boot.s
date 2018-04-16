c352e1d84b3dcbe2ba1ac9bbdd826956a969f5c0
Sam Tebbs <samuel.tebbs@gmail.com>
2018-04-16 22:45:32 +0100

    Rewrote major parts of boot.s and link.ld

--- src/kernel/boot.s
+++ src/kernel/boot.s
@@ -1,74 +1,62 @@
-;
-; boot.s -- Kernel start location. Also defines multiboot header.
-; Based on Bran's kernel development tutorial file start.asm
-;
+ALIGNMENT   equ 1
+MEM_INFO    equ 2
+MAGIC       equ 0x1BADB002
+FLAGS       equ ALIGNMENT | MEM_INFO
+CHECKSUM    equ -(MAGIC + FLAGS)
 
-MBOOT_PAGE_ALIGN    equ 1<<0    ; Load kernel and modules on a page boundary
-MBOOT_MEM_INFO      equ 1<<1    ; Provide your kernel with memory info
-MBOOT_HEADER_MAGIC  equ 0x1BADB002 ; Multiboot Magic value
-; NOTE: We do not use MBOOT_AOUT_KLUDGE. It means that GRUB does not
-; pass us a symbol table.
-MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
-MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+bits 32
 
-[BITS 32]                       ; All instructions should be 32-bit.
+section .multiboot
+align 4
+  dd  MAGIC
+  dd  FLAGS
+  dd  CHECKSUM
 
-[GLOBAL mboot]                  ; Make 'mboot' accessible from C.
-[EXTERN code]                   ; Start of the '.text' section.
-[EXTERN bss]                    ; Start of the .bss section.
-[EXTERN kernel_end]             ; End of the last loadable section.
+section .bss
+align 16
+kernel_stack:
+    resb 16384
+kernel_stack_end:
 
-mboot:
-  dd  MBOOT_HEADER_MAGIC        ; GRUB will search for this value on each
-                                ; 4-byte boundary in your kernel file
-  dd  MBOOT_HEADER_FLAGS        ; How GRUB should load your file / settings
-  dd  MBOOT_CHECKSUM            ; To ensure that the above values are correct
-
-  dd  mboot                     ; Location of this descriptor
-  dd  code                      ; Start of kernel '.text' (code) section.
-  dd  bss                       ; End of kernel '.data' section.
-  dd  kernel_end                ; End of kernel.
-  dd  start                     ; Kernel entry point (initial EIP).
-
-[GLOBAL start]                  ; Kernel entry point.
-[EXTERN kmain]                   ; This is the entry point of our C code
+section .text
+global start
+extern kmain
 
 start:
-  push    ebx                   ; Load multiboot header location
+    mov esp, kernel_stack_end
+    push ebx
+    call kmain
+    cli
+    .loop
+        hlt
+        jmp .loop
 
-  ; Execute the kernel:
-  cli                         ; Disable interrupts.
-  call kmain                   ; call our main() function.
-  jmp $                       ; Enter an infinite loop, to stop the processor
-                              ; executing whatever rubbish is in the memory
-                              ; after our kernel!
-
-[GLOBAL gdt_flush]    ; Allows the C code to call gdt_flush().
+global gdt_flush
 
 gdt_flush:
-   mov eax, [esp+4]  ; Get the pointer to the GDT, passed as a parameter.
-   lgdt [eax]        ; Load the new GDT pointer
+    mov eax, [esp + 4]
+    lgdt [eax]
 
-   mov ax, 0x10      ; 0x10 is the offset in the GDT to our data segment
-   mov ds, ax        ; Load all data segment selectors
-   mov es, ax
-   mov fs, ax
-   mov gs, ax
-   mov ss, ax
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-   ; Enter protected mode
-   mov eax, cr0
-   or al, 1
-   mov cr0, eax
+    ; Enter protected mode
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
 
-   jmp 0x08:.flush   ; 0x08 is the offset to our code segment: Far jump!
+    jmp 0x08:.flush
 .flush:
-   ret
+    ret
 
-[GLOBAL idt_flush]    ; Allows the C code to call idt_flush().
+global idt_flush
 
 idt_flush:
-   mov eax, [esp+4]  ; Get the pointer to the IDT, passed as a parameter.
-   lidt [eax]        ; Load the IDT pointer.
-   sti
-   ret
+    mov eax, [esp + 4]
+    lidt [eax]
+    sti
+    ret
