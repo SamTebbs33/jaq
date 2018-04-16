a0d56fd5e97faae19fe3b45a6146371a940ebe64
Sam Tebbs <samuel.tebbs@gmail.com>
2018-04-16 23:59:10 +0100

    Made kmalloc use kernel_heap if it is set

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -3,22 +3,35 @@
 //
 
 #include "mem.h"
+#include "paging.h"
+#include "heap.h"
 
 uint32_t placement_address;
+extern heap_t* kernel_heap;
+extern page_directory_t* kernel_directory;
 
 uint32_t _kmalloc(size_t size, bool align, uint32_t *phys) {
-    // If the address is not already page-aligned
-    if (align && (placement_address & 0xFFFFF000)) {
-        // Align it
-        placement_address &= 0xFFFFF000;
-        placement_address += 0x1000;
+    if(kernel_heap != NULL) {
+        void* addr = heap_alloc(size, kernel_heap);
+        if(phys) {
+            page_t* page = paging_get_page((uint32_t) addr, kernel_directory, FALSE);
+            *phys = page->frame * BYTES_PER_PAGE + ((uint32_t) addr & 0xFFF);
+        }
+        return (uint32_t) addr;
+    } else {
+        // If the address is not already page-aligned
+        if (align && (placement_address & 0xFFFFF000)) {
+            // Align it
+            placement_address &= 0xFFFFF000;
+            placement_address += 0x1000;
+        }
+        if (phys) {
+            *phys = placement_address;
+        }
+        uint32_t tmp = placement_address;
+        placement_address += size;
+        return tmp;
     }
-    if(phys) {
-        *phys = placement_address;
-    }
-    uint32_t tmp = placement_address;
-    placement_address += size;
-    return tmp;
 }
 
 uint32_t kmalloc(size_t size) {
