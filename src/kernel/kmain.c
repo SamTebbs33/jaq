daa6bbde4dc90f5e81e7f03bb01d08a1807a6f6d
Sam Tebbs <samuel.tebbs@gmail.com>
2018-04-17 01:03:53 +0100

    Added memory parsing to kmain

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -8,15 +8,19 @@
 #include "../drivers/keyboard.h"
 #include "../drivers/timer.h"
 #include "paging.h"
+#include "multiboot.h"
 
-void kmain() {
+void kmain(multiboot_info_t* mb_info) {
+    uint32_t total_mem = mb_info->mem_lower + mb_info->mem_lower;
+
     gdt_init();
     idt_init();
-    paging_init(0xF0000000); // Assume the RAM is 4GB big for now
-
+    paging_init(total_mem);
     keyboard_init();
     timer_init(50);
 
     print_clear();
-    PRINT("Hello, world!");
+    print("jaq OS, ");
+    print_u32(total_mem / 1024);
+    print("MB available");
 }
