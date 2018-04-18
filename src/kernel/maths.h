7f3c5a73904c6c326bcad6fe84cc0e80d1a3fee1
Sam Tebbs <samuel.tebbs@gmail.com>
2018-04-18 11:11:41 +0100

    Moved sign_i32 to maths.h

---
+++ src/kernel/maths.h
@@ -0,0 +1,17 @@
+//
+// Created by Samuel Tebbs on 18/04/2018.
+//
+
+#ifndef JAQ_MATHS_H
+#define JAQ_MATHS_H
+
+#include <stdinc.h>
+
+int8_t sign_i32(int32_t i);
+
+uint32_t max_u32(uint32_t a, uint32_t b);
+uint32_t min_u32(uint32_t a, uint32_t b);
+size_t max_size(size_t a, size_t b);
+size_t min_size(size_t a, size_t b);
+
+#endif //JAQ_MATHS_H
--- src/kernel/util.c
+++ src/kernel/util.c
@@ -18,7 +18,3 @@ void memset(void* buff, size_t len, char value) {
     char* buff2 = buff;
     for (size_t i = 0; i < len; i++) buff2[i] = value;
 }
-
-int8_t sign_i32(int32_t i) {
-    return (int8_t) (i < 0 ? -1 : (i == 0 ? 0 : 1));
-}
