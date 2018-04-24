6b0fbf24590e98a29b3b19034308094dbd718467
SamTebbs33 <samuel.tebbs@yahoo.com>
2018-04-24 10:49:56 +0100

    Added log_u32

--- src/kernel/log/log.c
+++ src/kernel/log/log.c
@@ -23,3 +23,14 @@ void log_error(char *msg) {
 void log_warning(char *msg) {
     log(LOG_LEVEL_WARNING, msg);
 }
+
+void log_u32_rec(uint32_t u32, bool first) {
+    if(u32 > 0) log_u32_rec(u32 / 10, false);
+    else if(!first) return;
+    uint8_t digit = (uint8_t) (u32 % 10);
+    serial_write(SERIAL_COM1_PORT, (char) (digit + '0'));
+}
+
+void log_u32(uint32_t u32) {
+    log_u32_rec(u32, true);
+}
