c6f097fe93252c264a6cd02ec5ce0d669ac1f29e
Sam Tebbs <samuel.tebbs@gmail.com>
2019-02-10 16:46:34 +0000

    F/string functions, part of #58
    F/string functions, part of #58

--- src/inc/util/string.h
+++ src/inc/util/string.h
@@ -15,8 +15,10 @@ char *strcpy(char *dest, const char *src);
 char *strncpy(char *dest, const char *src, size_t len);
 int strcmp(const char *s1, const char *s2);
 char* strtok(char* str, char* delim);
+char* strtok_r(char* str, char* delim, char** save);
 int strchr(char* str, char ch);
 int strrchr(char* str, char ch);
 char* strdup(char* str);
+char* strcat(char* dest, const char* str);
 
 #endif //JAQ_STRING_H
