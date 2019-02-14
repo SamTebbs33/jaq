b2f66de75cc33ac8da855f6978c0b88cb13f7883
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-02-14 22:51:08 +0000

    Add check for empty list

--- src/kernel/lib/sorted_linkedlist.c
+++ src/kernel/lib/sorted_linkedlist.c
@@ -5,6 +5,7 @@
 #include <lib/sorted_linkedlist.h>
 
 bool sorted_linkedlist_add(linkedlist_t *list, void *ptr, comparator_t cmp) {
+    if(linkedlist_size(list) == 0) return linkedlist_add(list, ptr);
     uint32_t i = 0;
     while (i < linkedlist_size(list) && cmp(ptr, linkedlist_get(list, i)) > 0) i++;
     return linkedlist_insert(list, ptr, i);
