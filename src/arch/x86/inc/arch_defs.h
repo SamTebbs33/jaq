5ce1c6f35a9cc9dd9633e29d9e73760645defdba
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-02-21 11:43:05 +0000

    Remove obsolete ARCH_INIT_PROCESS_STATE

--- src/arch/x86/inc/arch_defs.h
+++ src/arch/x86/inc/arch_defs.h
@@ -37,20 +37,4 @@ typedef arch_interrupt_handler_t arch_syscall_handler_t;
     asm("int $128");\
 }
 
-#define ARCH_INIT_PROCESS_STATE(process, entry_function, exit_function) {\
-    arch_cpu_state_t* state = process->cpu_state;\
-    uint32_t* process_kernel_stack = (uint32_t*)process->kernel_stack;\
-    state->ebp = (uint32_t)process_kernel_stack;\
-    state->esp = (uint32_t)process_kernel_stack + process->kernel_stack_size - 4*5;\
-    state->edi = 0;\
-    state->ebx = 0;\
-    state->esi = 0;\
-    process_kernel_stack[process->kernel_stack_size/4 - 5] = state->ebp;\
-    process_kernel_stack[process->kernel_stack_size/4 - 4] = state->edi;\
-    process_kernel_stack[process->kernel_stack_size/4 - 3] = state->esi;\
-    process_kernel_stack[process->kernel_stack_size/4 - 2] = state->ebx;\
-    process_kernel_stack[process->kernel_stack_size/4 - 1] = (uint32_t)entry_function;\
-    process_kernel_stack[process->kernel_stack_size/4] = (uint32_t)exit_function;\
-}
-
 #endif //JAQ_ARCH_TYPES_H
