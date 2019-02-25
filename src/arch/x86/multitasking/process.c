721a83608493db5187745b9f3fea88cffead5776
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-02-25 00:11:47 +0000

    Stop putting register values on initial process stack

--- src/arch/x86/multitasking/process.c
+++ src/arch/x86/multitasking/process.c
@@ -17,23 +17,15 @@ void arch_init_process_state(process_t* process, void (*entry_function)(void), v
 
     if (process->level == USER) {
         memset(user_state, 0, sizeof(arch_cpu_state_t));
-        kernel_state->useresp = (uint32_t)process->user_stack + process->user_stack_size - 4*5;
+        kernel_state->useresp = (uint32_t)process->user_stack + process->user_stack_size - 4;
         user_state->esp = kernel_state->useresp;
         uint32_t* user_stack = process->user_stack;
-        user_stack[process->user_stack_size/4 - 5] = user_state->edi;
-        user_stack[process->user_stack_size/4 - 4] = user_state->ebp;
-        user_stack[process->user_stack_size/4 - 3] = user_state->esi;
-        user_stack[process->user_stack_size/4 - 2] = user_state->ebx;
         user_stack[process->user_stack_size/4 - 1] = (uint32_t)entry_function;
         user_stack[process->user_stack_size/4] = (uint32_t)exit_function;
     }
 
     kernel_state->ebp = (uint32_t)process_kernel_stack;
-    kernel_state->esp = (uint32_t)process_kernel_stack + process->kernel_stack_size - 4*5;
-    process_kernel_stack[process->kernel_stack_size/4 - 5] = kernel_state->edi;
-    process_kernel_stack[process->kernel_stack_size/4 - 4] = kernel_state->ebp;
-    process_kernel_stack[process->kernel_stack_size/4 - 3] = kernel_state->esi;
-    process_kernel_stack[process->kernel_stack_size/4 - 2] = kernel_state->ebx;
+    kernel_state->esp = (uint32_t)process_kernel_stack + process->kernel_stack_size - 4;
     process_kernel_stack[process->kernel_stack_size/4 - 1] = (uint32_t)entry_function;
     process_kernel_stack[process->kernel_stack_size/4] = (uint32_t)exit_function;
 }
