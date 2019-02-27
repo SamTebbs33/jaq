a2643f7e119b54c1169826ee6b20532c87abe92c
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-02-27 13:54:33 +0000

    Add condition to USER level check in switch_to_next

--- src/kernel/multitasking/multitasking.c
+++ src/kernel/multitasking/multitasking.c
@@ -64,7 +64,7 @@ void switch_to_next(bool reschedule_current) {
         if(next_process->level == KERNEL) {
 		log_debug("Switching to kernel task\n");
 		arch_switch_to_kernel_task(tmp->kernel_state, current_process->kernel_state);
-	} else {
+	} else if(next_process->level == USER) {
 		log_debug("Switching to user task\n");
 		arch_switch_to_user_task(tmp->kernel_state, current_process->kernel_state, current_process->user_state);
 	}
