dde0857f29ec141f5b4ea37b278eda41e58408fc
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-02-27 14:45:18 +0000

    Fix mixed indent in multitasking.c

--- src/kernel/multitasking/multitasking.c
+++ src/kernel/multitasking/multitasking.c
@@ -62,12 +62,12 @@ void switch_to_next(bool reschedule_current) {
         process_t *tmp = current_process;
         current_process = next_process;
         if(next_process->level == KERNEL) {
-		log_debug("Switching to kernel task\n");
-		arch_switch_to_kernel_task(tmp->kernel_state, current_process->kernel_state);
-	} else if(next_process->level == USER) {
-		log_debug("Switching to user task\n");
-		arch_switch_to_user_task(tmp->kernel_state, current_process->kernel_state, current_process->user_state);
-	}
+                log_debug("Switching to kernel task\n");
+                arch_switch_to_kernel_task(tmp->kernel_state, current_process->kernel_state);
+        } else if(next_process->level == USER) {
+                log_debug("Switching to user task\n");
+                arch_switch_to_user_task(tmp->kernel_state, current_process->kernel_state, current_process->user_state);
+        }
     }
 }
 
