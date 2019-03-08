11643028ac4075069f7c08ebc6cd800b7f20c0b0
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-03-08 16:37:29 +0000

    Add colour escape sequences

--- src/kernel/fs/devfs.c
+++ src/kernel/fs/devfs.c
@@ -6,8 +6,13 @@
 #include <fs/fs.h>
 #include <screen/framebuffer.h>
 #include <lib/maths.h>
+#include <lib/string.h>
 #include <driver/serial.h>
 #include <driver/keyboard.h>
+#include <log/log.h>
+
+#define PRINT_ESCAPE_PREFIX "\\[\\033["
+#define PRINT_ESCAPE_SUFFIX "\\]"
 
 fs_node_t* dev, * stdout, * stderr, * serial, * stdin;
 unsigned int row = 0, column = 0;
@@ -23,8 +28,40 @@ void print_at(unsigned int r, unsigned int c) {
 }
 
 size_t stdout_write(fs_node_t* node, void* buff, size_t len, size_t offset) {
+    char* str = (char*)buff;
     for (size_t i = 0; i < len; ++i) {
-        char ch = ((char *) buff)[i];
+        if(strstr((char*)str + i, PRINT_ESCAPE_PREFIX) == 0) {
+            i += sizeof(PRINT_ESCAPE_PREFIX) - 1;
+            int end = strstr((char*)str + i, PRINT_ESCAPE_SUFFIX);
+            if(end >= 0) {
+                char* sequence = strndup((char*)str + i, end);
+                i += sizeof(PRINT_ESCAPE_SUFFIX) - 1 + end;
+                char* tok = strtok(sequence, ";");
+                char* next = strtok(NULL, ";");
+                bool secondary = false;
+                if(next) {
+                    if(strcmp(tok, "1") == 0) secondary = true;
+                    tok = next;
+                }
+                if(strcmp(tok, "30m") == 0) fg = secondary ? FB_DARK_GREY : FB_BLACK;
+                else if(strcmp(tok, "34m") == 0) fg = secondary ? FB_BLUE : FB_LIGHT_BLUE;
+                else if(strcmp(tok, "32m") == 0) fg = secondary ? FB_GREEN : FB_LIGHT_GREEN;
+                else if(strcmp(tok, "36m") == 0) fg = secondary ? FB_CYAN : FB_LIGHT_CYAN;
+                else if(strcmp(tok, "31m") == 0) fg = secondary ? FB_RED : FB_LIGHT_RED;
+                else if(strcmp(tok, "35m") == 0) fg = secondary ? FB_MAGENTA : FB_LIGHT_MAGENTA;
+                else if(strcmp(tok, "33m") == 0) fg = secondary ? FB_BROWN : FB_LIGHT_BROWN;
+                else if(strcmp(tok, "37m") == 0) fg = secondary ? FB_LIGHT_GREY : FB_WHITE;
+                else if(strcmp(tok, "44m") == 0) bg = secondary ? FB_BLUE : FB_LIGHT_BLUE;
+                else if(strcmp(tok, "42m") == 0) bg = secondary ? FB_GREEN : FB_LIGHT_GREEN;
+                else if(strcmp(tok, "46m") == 0) bg = secondary ? FB_CYAN : FB_LIGHT_CYAN;
+                else if(strcmp(tok, "41m") == 0) bg = secondary ? FB_RED : FB_LIGHT_RED;
+                else if(strcmp(tok, "45m") == 0) bg = secondary ? FB_MAGENTA : FB_LIGHT_MAGENTA;
+                else if(strcmp(tok, "43m") == 0) bg = secondary ? FB_BROWN : FB_LIGHT_BROWN;
+                else if(strcmp(tok, "47m") == 0) bg = secondary ? FB_LIGHT_GREY : FB_WHITE;
+            } else i -= sizeof(PRINT_ESCAPE_PREFIX) - 1; // Reset str
+        }
+        char ch = str[i];
+
         if (ch == PRINT_NEWLINE) print_at(row + 1, 0);
         else if (ch == PRINT_TAB) print_at(row, column + PRINT_TAB_SIZE);
         else {
