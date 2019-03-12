38be55026f1e6be55bd47a20774a833fe1ee370a
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-03-12 11:00:14 +0000

    Migrate save_cpu_state and restore_cpu_state to C

--- src/arch/x86/multitasking/multitasking.s
+++ src/arch/x86/multitasking/multitasking.s
@@ -4,45 +4,6 @@ useresp:
 usereip:
     .long 0
 
-# void arch_restore_cpu_state(arch_cpu_state_t* state) with state in eax
-.global arch_restore_cpu_state
-arch_restore_cpu_state:
-    # Restore the general purpose registers
-    mov 16(%eax), %edi
-    mov 20(%eax), %esi
-    mov 24(%eax), %ebp
-    mov 32(%eax), %ebx
-    mov 36(%eax), %edx
-    mov 40(%eax), %ecx
-    mov 44(%eax), %eax
-    ret
-
-# void arch_save_cpu_state(arch_cpu_state_t* state)
-.global arch_save_cpu_state
-arch_save_cpu_state:
-    # Save caller ebp
-    push %ebp
-    # Use local frame
-    mov %esp, %ebp
-    # Save ecx
-    push %ecx
-    # Get state to save into from the arguments. This is the 4th entry on the stack
-    mov 12(%esp), %ecx
-    # Save general purpose registers
-    mov %eax, 44(%ecx)
-    mov %edx, 36(%ecx)
-    mov %ebx, 32(%ecx)
-    mov %ebp, 24(%ecx)
-    mov %esi, 20(%ecx)
-    mov %edi, 16(%ecx)
-    # Pop off ecx into state
-    pop 40(%ecx)
-    # Clear local variables
-    mov %ebp, %esp
-    # Restore caller's ebp
-    pop %ebp
-    ret
-
 # void arch_switch_to_kernel_task(arch_cpu_state_t* current, arch_cpu_state_t* next)
 .global arch_switch_to_kernel_task
 arch_switch_to_kernel_task:
@@ -60,10 +21,11 @@ arch_switch_to_kernel_task:
     mov 28(%eax), %esp
     mov 24(%eax), %ebp
     mov %esp, (tss + 4)
-    # From now on we are using the next proc's stack
 
     # Restore kernel cpu state, we shouldn't touch any registers after this
+    push %eax
     call arch_restore_cpu_state
+    add $4, %esp
 
     # Re-enable interrupts
     sti
@@ -97,8 +59,10 @@ arch_switch_to_user_task:
     mov 56(%eax), %ebx
     mov %ebx, (usereip)
 
+    push %eax
     # Restore user cpu state, we shouldn't touch any registers after this
     call arch_restore_cpu_state
+    add $4, $esp
 
     # User data segment
     pushl $0x23
