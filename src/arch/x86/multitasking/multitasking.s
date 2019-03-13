ed7f92e8d1a2fd159a7bded327dadf0a1013afdd
SamTebbs33 <samuel.tebbs@yahoo.com>
2019-03-13 23:54:18 +0000

    Working on switching to user task

--- src/arch/x86/multitasking/multitasking.s
+++ src/arch/x86/multitasking/multitasking.s
@@ -45,45 +45,35 @@ arch_switch_to_kernel_task:
 # void arch_switch_to_user_task(arch_cpu_state_t* current, arch_cpu_state_t* next, arch_cpu_state_t* next_user)
 .global arch_switch_to_user_task
 arch_switch_to_user_task:
-    # Disable interrupts to avoid being interrupted mid-switch
     cli
-    # Get the current proc's state
-    mov 4(%esp), %edi
-    # Get the next proc's state
-    mov 8(%esp), %esi
-    # Get the next proc's user state
-    mov 12(%esp), %eax
-    # Changes to arch_cpu_state_t may require adjustments to the offset here
-    # Save the stack pointer into the current proc's state at arch_cpu_state_t.esp
-    mov %esp, 28(%edi)
-    mov %ebp, 24(%edi)
-    # Restore the stack pointer from the next proc's state from arch_cpu_state_t.esp
-    mov 28(%esi), %esp
-    mov 24(%esi), %ebp
-    # From now on we are using the next proc's kernel stack
-    mov %esp, (tss + 4)
-    # Save user.esp to temp storage
-    mov 28(%eax), %ebx
-    mov %ebx, (useresp)
-    # Save user.eip to temp storage
-    mov 56(%eax), %ebx
-    mov %ebx, (usereip)
-
+    push %edi
     push %eax
-    # Restore user cpu state, we shouldn't touch any registers after this
-    #call arch_restore_cpu_state
-    add $4, %esp
+    # Current proc's state
+    mov 12(%esp), %edi
+    # Next proc's state
+    mov 16(%esp), %eax
 
-    # User data segment
-    pushl $0x23
-    # User stack pointer
-    pushl (useresp)
-    # EFLAGS with interrupts re-enabled
-    pushl $0x202
-    # User code segment
-    pushl $0x1B
-    # Address to jump to
-    push (usereip)
+    # Save current state
+    pop 44(%edi)
+    pop 16(%edi)
+    mov %esi, 20(%edi)
+    mov %ebp, 24(%edi)
+    mov %esp, 28(%edi)
+    mov %ebx, 32(%edi)
+    mov %edx, 36(%edi)
+    mov %ecx, 40(%edi)
 
-    iret
+    # Restore next state
+    mov 16(%eax), %edi
+    mov 20(%eax), %esi
+    mov 24(%eax), %ebp
+    mov 28(%eax), %esp
+    mov 32(%eax), %ebx
+    mov 36(%eax), %edx
+    mov 40(%eax), %ecx
+    mov 44(%eax), %eax
+    mov %esp, (tss + 4)
 
+    # Interrupts are re-enabled by the interrupt handler
+    # Return
+    ret
--- src/arch/x86/multitasking/process.c
+++ src/arch/x86/multitasking/process.c
@@ -3,7 +3,10 @@
 //
 
 #include <multitasking/process.h>
+#include <gdt.h>
 #include <lib/util.h>
+
+extern void irq_return(void);
 
 void arch_copy_cpu_state(arch_cpu_state_t* dest, arch_cpu_state_t* src) {
     dest->edi = src->edi;
@@ -20,21 +23,39 @@ void arch_copy_cpu_state(arch_cpu_state_t* dest, arch_cpu_state_t* src) {
 void arch_init_process_state(process_t* process, void (*entry_function)(void), void (*exit_function)(void)) {
     arch_cpu_state_t* kernel_state = process->kernel_state;
     arch_cpu_state_t* user_state = process->user_state;
-    uint32_t* process_kernel_stack = (uint32_t*)process->kernel_stack;
+    uint32_t* kernel_stack = (uint32_t*)process->kernel_stack;
+    uint32_t kernel_stack_size = process->kernel_stack_size;
     memset(kernel_state, 0, sizeof(arch_cpu_state_t));
 
     if (process->level == USER) {
+        uint32_t* user_stack = (uint32_t*)process->user_stack;
+        uint32_t user_stack_size = process->user_stack_size;
         memset(user_state, 0, sizeof(arch_cpu_state_t));
-        kernel_state->useresp = (uint32_t)process->user_stack + process->user_stack_size;
-        user_state->esp = kernel_state->useresp;
-        user_state->ebp = user_state->esp - 4;
-        uint32_t* user_stack = process->user_stack;
-        user_state->eip = (uint32_t)entry_function;
-        user_stack[process->user_stack_size/4] = (uint32_t)exit_function;
-    }
 
-    kernel_state->esp = (uint32_t)process_kernel_stack + process->kernel_stack_size - 4;
+        user_state->ss = user_state->gs = user_state->fs = user_state->es = user_state->ds = GDT_DATA_SEGMENT_USER;
+        user_state->cs = GDT_CODE_SEGMENT_USER;
+        user_state->useresp = (uint32_t)process->user_stack + process->user_stack_size;
+        user_state->eflags = 0x200;
+        user_state->eip = (uint32_t)entry_function;
+
+        user_state->esp = user_state->useresp;
+        user_state->ebp = user_state->esp - 4;
+        user_stack[process->user_stack_size/4] = (uint32_t)exit_function;
+        // Exit return address
+        kernel_stack[kernel_stack_size / 4] = (uint32_t) exit_function;
+        // Put user state on stack
+        memcpy((uint32_t*)((uint32_t)kernel_stack + kernel_stack_size - 4 - sizeof(arch_cpu_state_t)), user_state, sizeof(arch_cpu_state_t));
+        logf(LOG_LEVEL_DEBUG, "copying to %d\n", (uint32_t)kernel_stack + kernel_stack_size - 4 - sizeof(arch_cpu_state_t));
+        // Dummy eax value
+        kernel_stack[kernel_stack_size / 4 - sizeof(arch_cpu_state_t) / 4 - 2] = 0;
+        // Put address of routine that pops off the user state and enters user mode at bottom of the stack
+        kernel_stack[kernel_stack_size / 4 - sizeof(arch_cpu_state_t) / 4 - 3] = (uint32_t) irq_return;
+        // Point kernel esp to below exit function address, user state and dummy eax value
+        kernel_state->esp = (uint32_t)kernel_stack + kernel_stack_size - sizeof(arch_cpu_state_t) - 3 * 4;
+    } else {
+        kernel_state->esp = (uint32_t)kernel_stack + kernel_stack_size - 4;
+        kernel_stack[kernel_stack_size/4 - 1] = (uint32_t)entry_function;
+        kernel_stack[kernel_stack_size/4] = (uint32_t)exit_function;
+    }
     kernel_state->ebp = kernel_state->esp - 4;
-    process_kernel_stack[process->kernel_stack_size/4 - 1] = (uint32_t)entry_function;
-    process_kernel_stack[process->kernel_stack_size/4] = (uint32_t)exit_function;
 }
